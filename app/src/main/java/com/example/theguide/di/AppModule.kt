445016eb2e66a4454d98e19a0cc0742b878f916a
Neslişah Çelek <neslisah.celek@outlook.com>
2024-04-17 15:06:22 +0300

    change imports

--- app/src/main/java/com/example/theguide/di/AppModule.kt
+++ app/src/main/java/com/example/theguide/di/AppModule.kt
@@ -3,9 +3,9 @@ package com.example.theguide.di
 import android.app.Application
 import com.example.theguide.data.manager.LocalUserManagerImpl
 import com.example.theguide.domain.manager.LocalUserManager
-import com.example.theguide.domain.usecase.AppEntryUseCases
-import com.example.theguide.domain.usecase.ReadAppEntryUseCase
-import com.example.theguide.domain.usecase.SaveAppEntryUseCase
+import com.example.theguide.domain.usecase.appentry.AppEntryUseCases
+import com.example.theguide.domain.usecase.appentry.ReadAppEntryUseCase
+import com.example.theguide.domain.usecase.appentry.SaveAppEntryUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
