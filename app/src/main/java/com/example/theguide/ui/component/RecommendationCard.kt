37ccf5cdde72cf3e3df2586e3d3b1c534188954e
Neslişah Çelek <neslisah.celek@outlook.com>
2024-04-17 14:21:36 +0300

    Update RecommendationCard.kt

--- app/src/main/java/com/example/theguide/ui/component/RecommendationCard.kt
+++ app/src/main/java/com/example/theguide/ui/component/RecommendationCard.kt
@@ -5,68 +5,95 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.twotone.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.theguide.R
+import com.example.theguide.ui.theme.retroColorPalette
+import com.example.theguide.ui.theme.yellow
 
 @Composable
-fun RecomCard(
-    placeName: String
+fun RecommendationCard(
+    image: Int,
+    name: String,
+    rating: String
 ) {
-    Card (
+    Card(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight(),
         shape = RoundedCornerShape(20.dp),
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surface
+            containerColor = Color.White
         )
     ) {
-        Column (
+        Column(
             modifier = Modifier.padding(10.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(
+                text = stringResource(id = R.string.card_title),
+                color = Color.DarkGray,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(bottom = 10.dp)
+            )
             Image(
-                painterResource(id = R.drawable.walkers),
+                painterResource(id = image),
                 contentScale = ContentScale.FillBounds,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(130.dp),
+                    .height(130.dp)
+                    .clip(RoundedCornerShape(20.dp)),
                 contentDescription = "Place Image"
             )
             Spacer(modifier = Modifier.height(10.dp))
 
-            Row (
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp),
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
-            ){
-                Text(text = placeName, style = MaterialTheme.typography.titleMedium)
-                Image(
-                    imageVector = Icons.TwoTone.Star,
-                    contentDescription = "Ratings",
+            ) {
+                Text(
+                    text = name,
+                    color = Color.Black,
+                    style = MaterialTheme.typography.titleMedium,
                 )
-
-
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Filled.Star,
+                        contentDescription = "Rating",
+                        tint = yellow,
+                        modifier = Modifier.size(18.dp),
+                    )
+                    Text(
+                        text = rating,
+                        color = Color.Black,
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
             }
         }
     }
@@ -74,8 +101,10 @@ fun RecomCard(
 
 @Preview
 @Composable
-fun RecomCardPreview() {
-    RecomCard(
-        placeName = "Walkers"
+fun RecommendationCardPreview() {
+    RecommendationCard(
+        image = R.drawable.understone,
+        name = "Understone Coffee",
+        rating = "4.5"
     )
 }
