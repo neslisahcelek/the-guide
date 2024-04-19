24e020e468a3d0d15c787c892532b10adb7479b7
Neslişah Çelek <neslisah.celek@outlook.com>
2024-04-19 18:36:39 +0300

    delete redundant imports

--- app/src/main/java/com/example/theguide/MainActivity.kt
+++ app/src/main/java/com/example/theguide/MainActivity.kt
@@ -2,23 +2,13 @@ package com.example.theguide
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.res.stringResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.example.theguide.presentation.navigation.TheGuideNavGraph
-import com.example.theguide.ui.component.PrimaryAppBar
 import com.example.theguide.ui.theme.TheGuideTheme
 import dagger.hilt.android.AndroidEntryPoint
 
