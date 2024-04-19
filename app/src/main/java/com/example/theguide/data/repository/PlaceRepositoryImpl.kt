5bfffbd51b9f307db5db777e19a0bfd757ba9002
Neslişah Çelek <neslisah.celek@outlook.com>
2024-04-19 20:12:33 +0300

    change return type to resource

--- app/src/main/java/com/example/theguide/data/repository/PlaceRepositoryImpl.kt
+++ app/src/main/java/com/example/theguide/data/repository/PlaceRepositoryImpl.kt
@@ -5,15 +5,19 @@ import com.example.theguide.data.remote.PlaceDto
 import com.example.theguide.data.remote.PlacesAPI
 import com.example.theguide.domain.model.Place
 import com.example.theguide.domain.repository.PlaceRepository
+import com.example.theguide.domain.util.Resource
 
 class PlaceRepositoryImpl constructor(
     private val placesAPI: PlacesAPI
-) : PlaceRepository{
-    override suspend fun getPlaces(): List<Place> {
-        try {
-            return placesAPI.getPlaces("id").toPlaceModel()
+) : PlaceRepository {
+    override suspend fun getPlaces(): Resource<List<Place>> {
+        return try {
+            Resource.Success(
+                placesAPI.getPlaces("id").toPlaceModel()
+            )
         } catch (e: Exception) {
-            throw e
+            e.printStackTrace()
+            Resource.Error("An error occurred")
         }
     }
 }
--- app/src/main/java/com/example/theguide/domain/repository/PlaceRepository.kt
+++ app/src/main/java/com/example/theguide/domain/repository/PlaceRepository.kt
@@ -1,7 +1,8 @@
 package com.example.theguide.domain.repository
 
 import com.example.theguide.domain.model.Place
+import com.example.theguide.domain.util.Resource
 
 interface PlaceRepository {
-    suspend fun getPlaces(): List<Place>
+    suspend fun getPlaces(): Resource<List<Place>>
 }
