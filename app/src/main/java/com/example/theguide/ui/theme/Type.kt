4b64747a2568341d6710f487c1954322c009467b
Neslişah Çelek <neslisah.celek@outlook.com>
2024-05-03 20:13:10 +0300

    add font

--- app/src/main/java/com/example/theguide/ui/theme/Type.kt
+++ app/src/main/java/com/example/theguide/ui/theme/Type.kt
@@ -2,11 +2,18 @@ package com.example.theguide.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.theguide.R
 
-// Set of Material typography styles to start with
+val AlegreyaFontFamily = FontFamily(
+    Font(R.font.alegreya_semibold, FontWeight.SemiBold),
+    Font(R.font.alegreyasans_medium, FontWeight.Medium),
+    Font(R.font.alegreyasans_regular, FontWeight.Normal),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -14,11 +21,10 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
@@ -29,6 +35,13 @@ val Typography = Typography(
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
 )
