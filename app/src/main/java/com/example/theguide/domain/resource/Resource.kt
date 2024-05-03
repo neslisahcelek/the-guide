8bd211f401bedf4bfbfb8a4a13eea55ddb725034
Neslişah Çelek <neslisah.celek@outlook.com>
2024-05-03 20:17:45 +0300

    move resource

--- app/src/main/java/com/example/theguide/domain/resource/Resource.kt
+++ app/src/main/java/com/example/theguide/domain/resource/Resource.kt
@@ -1,4 +1,4 @@
-package com.example.theguide.domain.util
+package com.example.theguide.domain.resource
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
