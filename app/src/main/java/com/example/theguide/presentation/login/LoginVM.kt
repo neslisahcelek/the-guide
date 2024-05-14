d64062a8cf0c1399d55636f98315f2e1249e3dd3
Neslişah Çelek <neslisah.celek@outlook.com>
2024-05-14 18:58:47 +0300

    fix firestore user duplication

--- app/src/main/java/com/example/theguide/presentation/login/LoginVM.kt
+++ app/src/main/java/com/example/theguide/presentation/login/LoginVM.kt
@@ -7,20 +7,24 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.theguide.data.local.UserDao
 import com.example.theguide.data.remote.UserInfo
+import com.example.theguide.domain.model.User
 import com.example.theguide.domain.resource.Resource
 import com.example.theguide.domain.usecase.place.CreateUserUseCase
 import com.example.theguide.domain.usecase.place.GetUserUseCase
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.stevdzasan.onetap.GoogleUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -38,15 +42,12 @@ class LoginVM @Inject constructor(
     val userId: LiveData<Resource<String>>
         get() = _userId
 
-    init {
-        checkUserLogin()
-    }
-
     fun onAction(action: LoginAction) {
         when (action) {
             is LoginAction.SaveAppEntry -> saveAppEntry()
             is LoginAction.CreateUser -> createUser(action.user)
             is LoginAction.TokenIdReceived -> saveTokenId(action.tokenId)
+            is LoginAction.CheckLoginStatus -> checkLoginStatus()
         }
     }
 
@@ -57,54 +58,71 @@ class LoginVM @Inject constructor(
     }
 
     private fun createUser(user: GoogleUser?) = CoroutineScope(IO).launch {
-        //userCollection.document(user, SetOptions.merge())
-
-        Log.d("LoginVM", "createUser: ${user?.givenName}")
-        userCollection.add(
-            com.example.theguide.domain.model.User(
-                id = "",
-                firstName = user?.givenName ?: "",
-                lastName = user?.familyName ?: "",
-                email = user?.email ?: "",
-                locale = user?.locale,
-                picture = user?.picture
-            )
-        ).addOnSuccessListener { userReference ->
-            saveUserToRoom(userReference, user)
-            Log.d("db success", "DocumentSnapshot written with ID: ${userReference.id}")
-        }.addOnFailureListener { e ->
-            Log.w("db error", "Error adding user", e)
-        }
-    }
-
-    private fun saveUserToRoom(ref: DocumentReference, user: GoogleUser?) {
-        CoroutineScope(IO).launch {
-            userDao.upsertUser(
-                com.example.theguide.data.local.UserEntity(
-                    id = ref.id,
-                    googleTokenId = state.value.tokenId,
-                    firstName = user?.givenName ?: "",
-                    lastName = user?.familyName ?: "",
-                    email = user?.email ?: ""
+        if (user != null) {
+            userCollection.document(user.email!!)
+                .set(
+                    User(
+                        id = "",
+                        firstName = user.givenName ?: "",
+                        lastName = user.familyName ?: "",
+                        email = user.email ?: "",
+                        locale = user.locale,
+                        picture = user.picture ?: ""
+                    ),
+                    SetOptions.merge()
                 )
-            )
+                .addOnSuccessListener { userReference ->
+                    saveUserToRoom(user)
+                    Log.d("db success", "Document written with ID: $userReference")
+                }.addOnFailureListener { e ->
+                    Log.w("db error", "Error adding user", e)
+                }
         }
     }
 
-    private fun checkUserLogin() {
-        var userId = ""
-
-        CoroutineScope(IO).launch(IO) {
-            userDao.getUser().let { userEntity ->
-                userId = userEntity.id
+    private fun saveUserToRoom(user: GoogleUser?) {
+        CoroutineScope(IO).launch {
+            try {
+                userDao.upsertUser(
+                    com.example.theguide.data.local.UserEntity(
+                        id = "",
+                        googleTokenId = state.value.tokenId,
+                        firstName = user?.givenName ?: "",
+                        lastName = user?.familyName ?: "",
+                        email = user?.email ?: "",
+                        imageUrl = user?.picture ?: ""
+                    )
+                )
+                updateLoginStatus()
+            } catch (e: Exception) {
+                Log.e("LoginVM", "Error saving user to Room: ${e.message}", e)
             }
         }
+    }
 
-        if (userId.isNotEmpty()) {
-            _state.update {
-                it.copy(
-                    isLoggedIn = true
-                )
+    private fun updateLoginStatus() {
+        _state.update {
+            it.copy(
+                isLoggedIn = true
+            )
+        }
+    }
+
+    private fun checkLoginStatus() {
+        CoroutineScope(IO).launch {
+            val userEntity = userDao.getUser()
+            Log.d("LoginVM", "checkLoginStatus: ${state.value.isLoggedIn}")
+
+            if (userEntity != null) {
+                Log.d("LoginVM", "user: ${userEntity.firstName}")
+
+                withContext(Main) {
+                    _state.update {
+                        it.copy(
+                            isLoggedIn = true
+                        )
+                    }
+                }
             }
         }
     }
