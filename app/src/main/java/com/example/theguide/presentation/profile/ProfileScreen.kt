8be13293f8c7a0c573e43f83d97496df7121b1a0
Neslişah Çelek <neslisah.celek@outlook.com>
2024-05-15 20:02:15 +0300

    complete profile design

--- app/src/main/java/com/example/theguide/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/theguide/presentation/profile/ProfileScreen.kt
@@ -1,17 +1,17 @@
 package com.example.theguide.presentation.profile
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -20,8 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,9 +29,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.theguide.R
+import com.example.theguide.data.local.UserEntity
 import com.example.theguide.presentation.navigation.Route
-import com.example.theguide.presentation.welcome.WelcomeAction
-import com.example.theguide.presentation.welcome.WelcomeState
 import com.example.theguide.ui.component.PrimaryTopAppBar
 import com.example.theguide.ui.theme.TheGuideTheme
 import com.example.theguide.ui.theme.softOrange
@@ -52,59 +51,111 @@ fun ProfileScreen(
                 )
             }
         ) { values ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(values)
-                    .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
-                    .background(MaterialTheme.colorScheme.surface),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(30.dp)
-            ) {
-                Spacer(Modifier.height(30.dp))
-                /*
-             Image(
-                 painter = painterResource(R.drawable.bg),
-                 contentScale = ContentScale.FillBounds,
-                 modifier = Modifier
-                     .width(180.dp)
-                     .height(180.dp)
-                     .clip(RoundedCornerShape(90.dp)), contentDescription = ""
-             )
-*/
+            Box {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(values)
+                        .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+                        .background(MaterialTheme.colorScheme.surface),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(40.dp)
+                ) {
+                    /*
+                    Image(
+                        painter = painterResource(R.drawable.bg),
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .padding(top = 40.dp)
+                            .width(180.dp)
+                            .height(180.dp)
+                            .clip(RoundedCornerShape(90.dp)), contentDescription = ""
 
-             AsyncImage(
-                 model = state.profileImage,
-                 contentDescription = "Profile Image",
-                 contentScale = ContentScale.FillBounds,
-                 modifier = Modifier
-                     .width(180.dp)
-                     .height(180.dp)
-                     .clip(RoundedCornerShape(90.dp))
-             )
-
-
-
-                Text(
-                    text = state.userName,
-                    color = softOrange,
-                    style = MaterialTheme.typography.titleLarge.copy(
-                        fontWeight = FontWeight(700),
-                        fontSize = 30.sp
                     )
-                )
+                    */
 
-                Spacer(Modifier.height(300.dp))
-                Button(onClick = {
-                    action.invoke(ProfileAction.Logout)
-                    navigate.invoke(Route.LoginScreen.route)
-                }) {
-                    Text(text = stringResource(id = R.string.signup_button)
+                    AsyncImage(
+                        model = state.profileImage,
+                        contentDescription = "Profile Image",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .padding(top = 40.dp)
+                            .width(180.dp)
+                            .height(180.dp)
+                            .clip(RoundedCornerShape(90.dp))
+                    )
+
+
+
+                    Text(
+                        text = state.userName,
+                        color = softOrange,
+                        style = MaterialTheme.typography.titleLarge.copy(
+                            fontWeight = FontWeight(700),
+                            fontSize = 30.sp
+                        )
+                    )
+
+                    ProfileRow(
+                        title = stringResource(id = R.string.profile_email),
+                        value = state.user?.email ?: ""
+                    )
+
+                    ProfileRow(
+                        title = stringResource(id = R.string.profile_city),
+                        value = state.city
                     )
                 }
 
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 30.dp)
+                ) {
+                    Button(
+                        onClick = {
+                            action.invoke(ProfileAction.Logout)
+                            navigate.invoke(Route.LoginScreen.route)
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.signup_button)
+                        )
+                    }
+                }
             }
         }
+    }
+}
+
+@Composable
+fun ProfileRow(title: String, value: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 26.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = title,
+            color = Color.Black,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight(300),
+                fontSize = 16.sp
+            )
+        )
+        Spacer(modifier = Modifier
+            .height(1.dp)
+            .fillMaxWidth()
+            .background(Color.LightGray))
+        Text(
+            text = value,
+            color = Color.Black,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight(500),
+                fontSize = 16.sp
+            )
+        )
     }
 }
 
@@ -114,7 +165,15 @@ fun ProfileScreenPreview() {
     TheGuideTheme {
         ProfileScreen(
             state = ProfileState(
-                userName = "Neslişah Çelek"
+                userName = "Neslişah Çelek",
+                user = UserEntity(
+                    firstName = "Neslişah",
+                    lastName = "Çelek",
+                    googleTokenId = "",
+                    imageUrl = "",
+                    id = "",
+                    email = "neslisah.celek@outlook.com"
+                )
             )
         )
     }
