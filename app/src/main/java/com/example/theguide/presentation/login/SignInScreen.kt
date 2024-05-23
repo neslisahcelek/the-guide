5fc365344cf8054c87b063451e57e9c89fcbabd4
Neslişah Çelek <neslisah.celek@outlook.com>
2024-05-23 11:03:26 +0300

    design google sign in button

--- app/src/main/java/com/example/theguide/presentation/login/SignInScreen.kt
+++ app/src/main/java/com/example/theguide/presentation/login/SignInScreen.kt
@@ -1,18 +1,22 @@
 package com.example.theguide.presentation.login
 
-import android.widget.Toast
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,13 +24,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.theguide.R
@@ -43,10 +50,9 @@ fun SignInScreen(
     state: SignInState,
     onSignInClick: () -> Unit = {}
 ) {
-    val context = LocalContext.current
     LaunchedEffect(key1 = state.signInError) {
         state.signInError?.let {
-            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            Log.d("SignIn", "Error: $it")
         }
     }
     Surface(modifier = Modifier.fillMaxSize()) {
@@ -100,41 +106,36 @@ fun SignInScreen(
 
                 Spacer(modifier = Modifier.height(15.dp))
 
-                Button(onClick = onSignInClick) {
+                Button(
+                    modifier = Modifier.width(Dp.Unspecified),
+                    onClick = onSignInClick,
+                    shape = ButtonDefaults.shape,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White,
+                        contentColor = Color(0xFF1F1F1F),
+                    ),
+                    contentPadding = PaddingValues(6.dp),
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = Color(0xFF747775),
+                    )
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .padding(vertical = 4.dp)
+                            .padding(start = 10.dp)
+                            .size(30.dp)
+                            .paint(
+                                painter = painterResource(id = R.drawable.google_logo),
+                                contentScale = ContentScale.Inside
+                            )
+                    )
                     Text(
+                        modifier = Modifier.padding(horizontal = 10.dp),
                         text = stringResource(id = R.string.sign_in_with_google),
-                        color = Color.White
+                        maxLines = 1
                     )
                 }
-                /*
-                val oneTapSignInState = rememberOneTapSignInState()
-
-                OneTapSignInWithGoogle(
-                    state = oneTapSignInState,
-                    rememberAccount = true,
-                    clientId = Util.WEB_CLIENT_ID,
-                    onTokenIdReceived = { tokenId ->
-                        Log.d("LOG", "tokenid received")
-                        action.invoke(LoginAction.TokenIdReceived(tokenId))
-                        action.invoke(LoginAction.CreateUser(getUserFromTokenId(tokenId)))
-                    },
-                    onDialogDismissed = {}
-                )
-
-                OneTapGoogleButton(
-                    clientId = Util.WEB_CLIENT_ID,
-                    onClick = { oneTapSignInState.open() },
-                    onUserReceived = {
-                        Log.d("LOG", "user received")
-                    },
-                    border = BorderStroke(
-                        width = 2.dp,
-                        color = softOrange,
-                    )
-                )
-
-                 */
-
             }
 
         }
