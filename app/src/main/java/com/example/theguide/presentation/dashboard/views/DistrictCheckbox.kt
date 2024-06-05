9766d39e4678892f4ca431fe8760e37f63928904
Neslişah Çelek <neslisah.celek@outlook.com>
2024-06-05 19:25:54 +0300

    add title

--- app/src/main/java/com/example/theguide/presentation/dashboard/views/DistrictCheckbox.kt
+++ app/src/main/java/com/example/theguide/presentation/dashboard/views/DistrictCheckbox.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
@@ -17,34 +18,59 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.theguide.presentation.dashboard.CheckboxState
+import com.example.theguide.ui.theme.AlegreyaFontFamily
 import com.example.theguide.ui.theme.TheGuideTheme
+import com.example.theguide.ui.theme.Typography
 import com.example.theguide.ui.theme.softOrange
 
 
 @Composable
 fun DistrictCheckbox(districts: SnapshotStateList<CheckboxState>) {
-    districts.forEachIndexed { index, info ->
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.surface)
-                .padding(vertical = 12.dp)
-                .padding(start = 8.dp, end = 20.dp)
-        ) {
-            Checkbox(
-                checked = info.isChecked,
-                onCheckedChange = { isChecked ->
-                    districts[index] = info.copy(isChecked = isChecked)
-                },
-                colors = CheckboxDefaults.colors(
-                    checkedColor = softOrange,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+        Text(
+            modifier = Modifier.padding(top=8.dp).align(Alignment.CenterHorizontally),
+            text = "Öneri Almak İstediğin Bölgeyi Seç",
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            style = Typography.titleSmall.copy(
+                fontSize = 18.sp,
+                fontWeight = FontWeight(500),
+            ),
+        )
+        districts.forEachIndexed { index, info ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surface)
+                    .padding(vertical = 4.dp)
+                    .padding(start = 8.dp, end = 20.dp)
+            ) {
+                Checkbox(
+                    checked = info.isChecked,
+                    onCheckedChange = { isChecked ->
+                        districts[index] = info.copy(isChecked = isChecked)
+                    },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = softOrange,
+                    )
                 )
-            )
-            Text(text = info.text, color = Color.Black, modifier = Modifier.padding(start = 6.dp))
+                Text(
+                    text = info.text,
+                    color = Color.Black,
+                    modifier = Modifier.padding(start = 6.dp)
+                )
+            }
         }
     }
 }
