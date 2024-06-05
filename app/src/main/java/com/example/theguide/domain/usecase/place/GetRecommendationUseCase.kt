e4b5b827c5894391e704de81588b2b9bd8d3b7fc
Neslişah Çelek <neslisah.celek@outlook.com>
2024-06-05 19:25:05 +0300

    update recommendation limit

--- app/src/main/java/com/example/theguide/domain/usecase/place/GetRecommendationUseCase.kt
+++ app/src/main/java/com/example/theguide/domain/usecase/place/GetRecommendationUseCase.kt
@@ -8,7 +8,7 @@ class GetRecommendationUseCase @Inject constructor(
 ) {
     suspend fun execute(
         userId: String,
-        recommendationLimit: Int = 5,
+        recommendationLimit: Int = 15,
         districtList: List<String> = listOf()
     ) = placeRepository.getRecommendation(
         userId = userId,
